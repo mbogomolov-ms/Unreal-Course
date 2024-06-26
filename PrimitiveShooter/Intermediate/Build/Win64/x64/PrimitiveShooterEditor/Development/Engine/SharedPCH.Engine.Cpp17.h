c69a7c849f0edd270efbe1e12306d46d616652b6
MikhailBogomolov <mikhaillbogomolov@gmail.com>
2024-06-26 16:23:24 +0200

    -uploaded last part of PrmitiveShooter - game that was developed to study shooter and animation basics

---
+++ PrimitiveShooter/Intermediate/Build/Win64/x64/PrimitiveShooterEditor/Development/Engine/SharedPCH.Engine.Cpp17.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Work/UnrealProjects/PrimitiveShooter/Intermediate/Build/Win64/x64/PrimitiveShooterEditor/Development/Engine/SharedDefinitions.Engine.Cpp17.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
---
+++ PrimitiveShooter/Intermediate/Build/Win64/x64/PrimitiveShooterEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Engine/Public/EngineSharedPCH.h
+#include "C:/Work/UnrealProjects/PrimitiveShooter/Intermediate/Build/Win64/x64/PrimitiveShooterEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
+#include "Runtime/Engine/Public/EngineSharedPCH.h"
